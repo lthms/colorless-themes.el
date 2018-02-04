c9ed4e6c8c676338c27fa62391025bb4d50b231d
Thomas Letan <contact@thomasletan.fr>
2018-02-04 09:48:19 +0100

    chore: ;;; has a special meaning in elisp, use ;; instead

--- nordless-theme.el
+++ nordless-theme.el
@@ -1,33 +1,36 @@
 ;;; nordless-theme.el --- A mostly colorless theme
-;;; Copyright (C) 2018 Thomas Letan
-;;;
-;;; This program is free software: you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation, either version 3 of the License, or
-;;; (at your option) any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-;;; Author: Thomas Letan <contact@thomasletan.fr>
-;;; URL: https://github.com/lethom/nordless-theme.el
-;;; Version: 0.1
-;;; License: GPL-3
-;;; Keywords: theme, dark
+;; Copyright (C) 2018 Thomas Letan
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Thomas Letan <contact@thomasletan.fr>
+;; URL: https://github.com/lethom/nordless-theme.el
+;; Version: 0.1
+;; License: GPL-3
+;; Keywords: theme, dark
 
 ;;; Commentary:
-;;; nordless has two main source of inspiration: nofrils[1], an extremely
-;;; minimalist colorscheme for vim, and nord[2], a north-bluish color palette.
-;;;
-;;; [1]: https://github.com/robertmeta/nofrils
-;;; [2]: https://github.com/arcticicestudio/nord
+
+;; nordless has two main source of inspiration: nofrils[1], an extremely
+;; minimalist colorscheme for vim, and nord[2], a north-bluish color palette.
+;;
+;; [1]: https://github.com/robertmeta/nofrils
+;; [2]: https://github.com/arcticicestudio/nord
 
 ;;; Code:
+
 (deftheme nordless "A mostly colorless theme")
 
 (let ((nord0   "#2E3440") ; polar night
@@ -185,7 +188,7 @@
    )
   )
 
-;;; Autoload:
+;;;###autoload
 (when load-file-name
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))
