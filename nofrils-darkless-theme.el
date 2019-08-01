4a8225a94f65df94ccb2bb15c5d76fabbb17a948
Thomas Letan <contact@thomasletan.fr>
2019-08-01 21:01:24 +0200

    new: Introduce nofrils-darkless-theme
    This is a clone of nofrils, which feels kind of special since nofrils
    is the theme that motivated me to try colorless themes.

---
+++ nofrils-darkless-theme.el
@@ -0,0 +1,49 @@
+;;; nofrils-darkless-theme.el --- Port of nofrils-dark using colorless-themes
+
+;; Copyright (C) 2019 Thomas Letan
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Thomas Letan <contact@thomasletan.fr>
+;; URL: https://git.sr.ht/~lthms/colorles-themes.el
+;; Version: 0.1
+;; Package-Requires: ((colorless-themes "0.1"))
+;; License: GPL-3
+;; Keywords: theme
+
+;;; Commentary:
+;; This is a port of nofrils-dark, which is the original theme that motivated me
+;; to try “mostly colorless themes” to begin with.
+;;
+;; https://github.com/robertmeta/nofrils
+
+;;; Code:
+(require 'colorless-themes)
+
+(colorless-themes-make nofrils-darkless
+                       "#303030"    ; bg
+                       "#262626"    ; bg+
+                       "#404040"    ; current-line
+                       "#6C6C6C"    ; fade
+                       "#EEEEEE"    ; fg
+                       "#D4D4D4"    ; fg2
+                       "#EFEFEF"    ; fg3
+                       "#AF5FFF"    ; primary
+                       "#FF0000"    ; red
+                       "#FFBF00"    ; orange
+                       "#FF7000"    ; yellow
+                       "#00FF00")   ; green
+
+(provide 'nofrils-darkless-theme)
+;;; nofrils-darkless-theme.el ends here
